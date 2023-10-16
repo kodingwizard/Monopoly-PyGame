f35d3281259ae2de4318722cb1abe1eca52d5459
Archit-Das <dgr8archit@gmail.com>
2023-10-16 11:24:17 -0700

    Update monopoly_board.py

--- monopoly_board.py
+++ monopoly_board.py
@@ -1,9 +1,12 @@
 import turtle as t
 from random import random
 t.up()
-t.goto(-300,-300)
-#t.forward(-200)
-#t.down()
+t.goto(-300, -300)
+t.down()
+t.goto(300, -300)
+t.goto(300, 300)
+t.goto(-300, 300)
+t.goto(-300, -300)
 #for i in range(4):
 #    t.forward(500)
 #    t.left(90)
