9b26bfc9958a1f23bb7153d88165fe4dfb511a3f
Archit-Das <dgr8archit@gmail.com>
2023-10-17 11:30:35 -0700

    Update monopoly_board.py

--- monopoly_board.py
+++ monopoly_board.py
@@ -13,10 +13,11 @@ t.goto(-455, -455)
 width = 70
 run = 0
 t.goto(-455, 315)
+t.forward(70)
 for i in range(4):
    run = run + 1
    #t.up()
-   t.goto(-455,300-(width*run))\
+   t.goto(-455,315-(width*run))\
    #t.down()
    t.forward(70)
 '''
