4c56c02c063a7b7ea1809063daa99d5523166212
kodingwizard <nandinim.dharwadkar@gmail.com>
2023-10-17 11:53:42 -0700

    Update monopoly_board.py

--- monopoly_board.py
+++ monopoly_board.py
@@ -1,5 +1,6 @@
 import turtle as t
 from random import random
+t.speed(0)
 t.up()
 t.goto(-455, -455)
 t.down()
@@ -13,36 +14,46 @@ t.goto(-455, -455)
 width = 70
 run = 0
 t.goto(-455, 315)
-t.forward(160)
+t.forward(140)
 for i in range(9):
    run = run + 1
    t.up()
    t.goto(-455,315-(width*run))
    t.down()
-   t.forward(160)
+   t.forward(140)
 t.up()
 t.goto(455, 315)
 t.down()
-t.forward(-160)
+t.forward(-140)
+run = 0
 for i in range(9):
    run = run + 1
    t.up()
    t.goto(455,315-(width*run))
    t.down()
-   t.forward(-160)
-'''
-#t.forward(70)
-#t.up()
-t.goto(-455, 265)
-t.down()
-t.forward(70)
+   t.forward(-140)
 t.up()
-t.goto(-455, 230)
+t.goto(-315, 455)
 t.down()
-t.forward(70)
+t.left(-90)
+t.forward(140)
+run = 0
+for i in range(9):
+   run = run + 1
+   t.up()
+   t.goto(-315+(width*run), 455)
+   t.down()
+   t.forward(140)
 t.up()
-t.goto(-455, 195)
+t.goto(-315, -455)
 t.down()
-t.forward(70)
-'''
+t.left(180)
+t.forward(140)
+run = 0
+for i in range(9):
+   run = run + 1
+   t.up()
+   t.goto(-315+(width*run), -455)
+   t.down()
+   t.forward(140)
 t.mainloop()
