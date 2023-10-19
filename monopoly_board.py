bc8215a7f7c36b51c432f16675d8244d7bd28868
kodingwizard <nandinim.dharwadkar@gmail.com>
2023-10-19 11:36:08 -0700

    delete text and banner

--- monopoly_board.py
+++ monopoly_board.py
@@ -87,19 +87,6 @@ t.shapesize(10,5)
 t.tilt(0)
 t.fillcolor("skyblue")
 
-t.clone()
-#Center Monopoly Banner
-t.goto(0,0)
-t.shape("square")
-t.shapesize(33,5)
-t.tilt(0)
-t.fillcolor("red")
-
-t.clone()
-#t.down()
-#Center Title
-t.goto(0,0)
-t.write("MONOPOLY", font = ("RifficFree-Bold", 30, "normal"))
 
 
 
