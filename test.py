e0d4da925c34c6e58c2464c011115d9529d08244
kodingwizard <nandinim.dharwadkar@gmail.com>
2023-10-23 11:36:19 -0700

    py

---
+++ test.py
@@ -0,0 +1,60 @@
+import pygame as py
+
+
+py.init()
+
+
+SCREEN_WIDTH = 1925
+SCREEN_HEIGHT = 1000
+
+
+screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), py.RESIZABLE)
+
+
+def board():
+    py.draw.rect(screen, (175, 225, 175), (100, 50, 910, 910))
+    py.draw.rect(screen, (0, 0 , 0), (100, 50, 140, 140), 2)
+    run = 0
+    for i in range(9):
+        width = 70
+        py.draw.rect(screen, (0, 0, 0), (100, 190+(width * run), 140, 70), 2)
+        run = run + 1
+    py.draw.rect(screen, (0, 0 , 0), (100, 820, 140, 140), 2)
+    run = 0
+    for i in range(9):
+        width = 70
+        py.draw.rect(screen, (0, 0, 0), (240+(width*run), 820, 70, 140), 2)
+        run = run + 1
+    py.draw.rect(screen, (0, 0, 0), (870, 820, 140, 140), 2)
+    run = 0
+    for i in range(9):
+        width = 70
+        py.draw.rect(screen, (0, 0, 0), (870, 750-(width*run), 140, 70), 2)
+        run = run + 1
+    py.draw.rect(screen, (0, 0, 0), (870, 50, 140, 140), 2)
+    run = 0
+    for i in range(9):
+        width = 70
+        py.draw.rect(screen, (0, 0, 0), (240+(width*run), 50, 70, 140), 2)
+        run = run + 1
+   
+monopolyIMG = py.image.load('monopolycenter.gif')
+
+
+run = True
+while run:
+    screen.fill((255, 255, 255))
+    board()
+
+
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            run = False
+
+
+    py.display.update()
+
+
+py.quit()
+
+
