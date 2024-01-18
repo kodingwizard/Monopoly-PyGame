1ffd94800a179822261023c1a928d724c9681ab7
Archit-Das <dgr8archit@gmail.com>
2024-01-18 12:38:09 -0800

    pls wokr

--- mnply_board.py
+++ mnply_board.py
@@ -266,6 +266,12 @@ while run:
     if (((800 < player2.x < 870) and (820 < player2.y < 960)) or ((100 < player2.x < 240) and (50 < player2.y < 120)) or ((870 < player2.x < 1010) and (330 < player2.y < 470))) and (end_turn == 1):
         comcard = py.image.load("ComCard/comone.png")
         screen.blit(comcard, (400, 500))
+<<<<<<< Updated upstream
+=======
+    if(((380 < player2.x < 450) and (820 < player2.y < 960)) or ((310 < player2.x < 380) and (50 < player2.y < 190)) or ((870 < player2.x < 1100) and (520 < player2.y < 590))) and (end_turn == 1):
+        chance_test = py.image.load("ChanceCards/chancecard.png")
+        screen.blit(chance_test, (100,100))
+>>>>>>> Stashed changes
 
     if endturn.draw(screen):
         end_turn = 0
