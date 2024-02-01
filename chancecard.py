e2596c17e84b48c6f7c8d6512e286fe4f4d14eb2
Archit-Das <dgr8archit@gmail.com>
2024-02-01 12:59:34 -0800

    ......

--- chancecard.py
+++ chancecard.py
@@ -143,7 +143,7 @@ def chancecardp1():
     from mnply_board import end_turn
     from mnply_board import turn
     global chancecounterp1
-    if(((380 < player1.x < 450) and (820 < player1.y < 960)) or ((310 < player1.x < 380) and (50 < player1.y < 190)) or ((870 < player1.x < 1100) and (520 < player1.y < 590))) and (end_turn == 1) and (turn % 2 == 0):
+    if(((380 < player1.x < 450) and (820 < player1.y < 960)) or ((310 < player1.x < 380) and (50 < player1.y < 190)) or ((870 < player1.x < 1010) and (540 < player1.y < 610))) and (end_turn == 1) and (turn % 2 == 0):
         if chancecounterp1 == 0:
             random.choice(chancelist)()
             chancecounterp1 = 1
@@ -158,7 +158,7 @@ def chancecardp2():
     from mnply_board import end_turn
     from mnply_board import turn
     global chancecounterp2
-    if(((380 < player2.x < 450) and (820 < player2.y < 960)) or ((310 < player2.x < 380) and (50 < player2.y < 190)) or ((870 < player2.x < 1100) and (520 < player2.y < 590))) and (end_turn == 1) and (turn % 2 == 0):
+    if(((380 < player2.x < 450) and (820 < player2.y < 960)) or ((310 < player2.x < 380) and (50 < player2.y < 190)) or ((870 < player2.x < 1010) and (540 < player2.y < 610))) and (end_turn == 1) and (turn % 2 == 0):
         if chancecounterp2 == 0:
             random.choice(chancelist)()
             chancecounterp2 = 1
